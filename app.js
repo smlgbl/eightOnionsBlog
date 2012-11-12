184d70efd00ccfa936ce3df15ba5ea6ac0c95cd5
Samuel Gabel <smlgbl@gmail.com>
2012-11-12 21:23:26 +0100

    Going back to the original and do more by hand.

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
-var express = require('express')
-var ArticleProvider = require('./articleProviderInMemory').ArticleProvider
+/* Module deps */
+
+var express = require( 'express' )
+  , stylus = require( 'stylus' )
+  , nib = require( 'nib' )
 
 var app = express()
+function  compile( str, path ) {
+  return stylus( str )
+    .set( 'filename', path )
+	.use( nib() )
+}
 
-app.configure(function(){
-  app.set('views', __dirname + '/views')
-  app.set('view engine', 'jade')
-  app.use(express.bodyParser())
-  app.use(express.methodOverride())
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }))
-  app.use(app.router)
-  app.use(express.static(__dirname + '/public'))
+// data
+function Post( title, content ) {
+  this.title = title
+  this.content = content
+}
+
+var posts = [ 
+    new Post( 'Testing node.js', 'Text of post 1 isn\'t that long. But it is available' )
+  , new Post( 'Post #2', 'Text of post 1 isn\'t that long. But it is available' )
+  , new Post( 'Post #3', 'Text of post 1 isn\'t that long. But it is available' )
+  , new Post( 'Post #4', 'Text of post 1 isn\'t that long. But it is available' )
+]
+
+app.set( 'views', __dirname + '/views' )
+app.set( 'view engine', 'jade' )
+app.use( express.logger( 'dev' ) )
+app.use( stylus.middleware( 
+  {
+    src: __dirname + '/public' 
+	, compile: compile
+  }
+))
+app.use( express.static( __dirname + '/public' ) )
+
+app.get( '/', function( req, res ) {
+  res.render( 'index',
+  {
+      title : 'eightOnions.com'
+    , posts : posts
+  }
+  )
 })
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
-})
-
-app.configure('production', function(){
-  app.use(express.errorHandler())
-})
-
-var articleProvider= new ArticleProvider()
-
-app.get('/', function(req, res){
-  articleProvider.findAll(function(error, docs){
-      res.render('index.jade', { locals: {
-          title: 'eightOnions blog',
-          articles:docs
-          }
-      })
-  })
-})
-
-app.listen(3000)
+app.listen( 80 )
