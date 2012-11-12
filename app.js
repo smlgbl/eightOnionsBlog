891454b1b1c59c233fd94ba06d81b852dc5435e8
sml <sml@v10-s141.netcup.net>
2012-11-12 18:15:50 +0100

    Usable now.

--- app.js
+++ app.js
@@ -1,47 +1,32 @@
+var express = require('express');
+var ArticleProvider = require('./articleProviderInMemory').ArticleProvider;
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
-
-var ArticleProvider = require('./articleProviderInMemory').ArticleProvider
-
-var app = express()
+var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
-})
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
-})
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
 app.configure('production', function(){
-  app.use(express.errorHandler())
+  app.use(express.errorHandler());
+});
+
+var articleProvider= new ArticleProvider();
+
+app.get('/', function(req, res){
+  articleProvider.findAll(function(error, docs){
+      res.send(docs);
+  });
 })
 
-var articleProvider = new ArticleProvider()
-
-app.get('/', funcion(req, res) {
-  articleProvider.findAll( function(error, docs) {
-    res.send(docs)
-  })
-}
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'))
-})
+app.listen(3000);
