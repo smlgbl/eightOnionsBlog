3ea3fbb3952a03ecb17331691c088ffdab736104
Samuel Gabel <smlgbl@gmail.com>
2012-11-14 01:41:06 +0100

    Added a login functionality - views, register still missing

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 /* Module deps */
 
 var express = require( 'express' )
-  , stylus = require( 'stylus' )
-  , nib = require( 'nib' )
-  , ArticleProvider = require('./articleProviderMongoDB.js').ArticleProvider
+, stylus = require( 'stylus' )
+, nib = require( 'nib' )
+, ArticleProvider = require('./modules/articleProviderMongoDB.js').ArticleProvider
+, accountManager = require('./modules/accountManager.js')
+, emailDispatcher = require('./modules/emailDispatcher.js')
 
 var app = express()
 function  compile( str, path ) {
   return stylus( str )
-    .set( 'filename', path )
-	.use( nib() )
+  .set( 'filename', path )
+  .use( nib() )
 }
 
 app.set( 'views', __dirname + '/views' )
 app.set( 'view engine', 'jade' )
 app.use( express.logger( 'dev' ) )
 app.use( express.bodyParser() )
+app.use( express.cookieParser() )
 app.use( stylus.middleware( 
-  {
-    src: __dirname + '/public' 
-	, compile: compile
-  }
+{
+  src: __dirname + '/public' 
+  , compile: compile
+}
 ))
 app.use( express.static( __dirname + '/public' ) )
 
@@ -30,52 +33,101 @@ app.get( '/', function( req, res ) {
   articleProvider.findAll( 
     function( error, articles) {
       res.render( 'index',
-        {
-          title : 'eightOnions.com'
+      {
+        title : 'eightOnions.com'
         , posts : articles
-        }
+      }
       )
     }
-  )
+    )
 })
 
-app.get('/blog/new', function(req, res) {
-    res.render('blog_new.jade', {
-      title: 'New Post'
-    })
-})
-
-app.post('/blog/new', function(req, res){
-    articleProvider.save(
-      [{
-        title: req.param('title'),
-        body: req.param('body')
-      }],
-      function( error, articles ) {
-        res.redirect('/')
+app.get('/admin', function( req, res ) {
+  if ( req.cookies.user == undefined || req.cookies.pass == undefined ) {
+    res.render('admin_login', { title: "Hi, please login:"})
+  } else {
+    accountManager.autoLogin( req.cookies.user, req.cookies.pass, function( user ) {
+      if ( user != null ) {
+        req.session.user = user
+        res.redirect('/admin/home')
+      } else {
+        res.render( 'admin_login', { title: "Hi, please login:"})
       }
+    })
+  }
+})
+
+app.post('/admin', function( req, res ) {
+  if ( req.param('email') != null ) {
+    accountManager.getEmail( req.param('email'), function( user ) {
+      if ( user ) {
+        res.send( 'ok', 200 )
+        emailDispatcher.send( user, function(e, m){
+          console.log('error : '+e, 'msg : '+m)
+        })
+      } else {
+        res.send( 'email-not-found', 400 )
+      }
+    } )
+  } else {
+    accountManager.manualLogin( req.param('user'), req.param('pass'), function(e, user) {
+      if ( !user ) {
+        res.send( e, 400 )
+      } else {
+        req.session.user = user
+        if ( req.param('remember-me') == 'true' ) {
+          res.cookie('user', user.user, { maxAge: 900000 })
+          res.cookie('pass', user.pass, { maxAge: 900000 })
+        }
+        res.send( user, 200 )
+      }
+    })
+  }
+})
+
+app.get('/admin/home', function(req, res) {
+  res.render('admin_home.jade', {
+    title: 'Welcome!'
+  })
+})
+
+app.get('/admin/new', function(req, res) {
+  res.render('blog_new.jade', {
+    title: 'New Post'
+  })
+})
+
+app.post('/admin/new', function(req, res){
+  articleProvider.save(
+    [{
+      title: req.param('title'),
+      body: req.param('body')
+    }],
+    function( error, articles ) {
+      res.redirect('/')
+    }
     )
 })
 
 app.get('/blog/:id', function(req, res) {
-    articleProvider.findById(req.params.id, function(error, article) {
-        res.render('blog_show.jade',
-          {
-            title: article.title,
-            article:article
-          }
-        )
-    })
+  articleProvider.findById(req.params.id, function(error, article) {
+    res.render('blog_show.jade',
+    {
+      title: article.title,
+      article:article
+    }
+    )
+  })
 })
 
-app.post('/blog/addComment', function(req, res) {
-    articleProvider.addCommentToArticle(req.param('_id'), {
-        person: req.param('person'),
-        comment: req.param('comment'),
-        created_at: new Date()
-       } , function( error, docs) {
-           res.redirect('/blog/' + req.param('_id'))
-       })
-})
+// app.post('/blog/addComment', function(req, res) {
+//     articleProvider.addCommentToArticle(req.param('_id'), {
+//         person: req.param('person'),
+//         comment: req.param('comment'),
+//         created_at: new Date()
+//        } , function( error, docs) {
+//            res.redirect('/blog/' + req.param('_id'))
+//        })
+// })
 
-app.listen( 3000 )
+app.listen( 80 )
